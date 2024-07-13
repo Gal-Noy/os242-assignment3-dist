ee4145cf519c0c859c74582f924b0ecd2dd97d1a
Gal Noy <109943831+Gal-Noy@users.noreply.github.com>
2024-07-13 15:55:26 +0000

    more checks

--- user/crypto_srv.c
+++ user/crypto_srv.c
@@ -8,6 +8,9 @@
 
 #include "kernel/crypto.h"
 
+#define REASONABLE_DATA_SIZE 1024
+#define REASONABLE_KEY_SIZE 16
+
 int main(void)
 {
   if (open("console", O_RDWR) < 0)
@@ -41,20 +44,27 @@ int main(void)
     op = (struct crypto_op *)addr;
 
     // Check if the operation is valid
-    if (op->state != CRYPTO_OP_STATE_INIT)
+    if (op->state != CRYPTO_OP_STATE_INIT ||                                                                              // Check if the state is initialized
+        (op->type != CRYPTO_OP_TYPE_ENCRYPT && op->type != CRYPTO_OP_TYPE_DECRYPT) ||                                     // Check if the type is valid
+        op->data_size > REASONABLE_DATA_SIZE || op->data_size <= 0 || op->data_size > size - sizeof(struct crypto_op) || // Check if the data size is valid
+        op->key_size > REASONABLE_KEY_SIZE)                                                                               // Check if the key size is valid
     {
       op->state = CRYPTO_OP_STATE_ERROR;
     }
     else
     {
-      // Decrypt or Encrypt the payload
-      for (int i = 0; i < op->data_size; i++)
+
+      if (op->key_size > 0)
       {
-        op->payload[i + op->key_size] ^= op->payload[i % op->key_size];
+        // Decrypt or Encrypt the payload
+        for (int i = 0; i < op->data_size; i++)
+        {
+          op->payload[i + op->key_size] ^= op->payload[i % op->key_size];
+        }
       }
 
       // Memory barrier (fence) to ensure that the data is written back to memory before setting the state to done
-      asm volatile("fence rw,rw" : : : "memory"); 
+      asm volatile("fence rw,rw" : : : "memory");
 
       // Set the state to done
       op->state = CRYPTO_OP_STATE_DONE;
